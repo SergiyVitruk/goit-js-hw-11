c0f10e78759c2f9a8e90e2f29001769373a4659e
SergiyVitruk <vitruk.sergiy@gmail.com>
2025-06-16 19:29:29 +0300

    start project

--- src/main.js
+++ src/main.js
@@ -12,37 +12,37 @@ import 'izitoast/dist/css/iziToast.min.css';
 const form = document.querySelector('.form');
 const input = form.querySelector('input[name="search-text"]');
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const query = input.value.trim();
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const query = input.value.trim();
 
-  if (!query) {
-    iziToast.warning({
-      message: 'Please enter a search query.',
-    });
-    return;
-  }
+//   if (!query) {
+//     iziToast.warning({
+//       message: 'Please enter a search query.',
+//     });
+//     return;
+//   }
 
-  clearGallery();
-  showLoader();
+//   clearGallery();
+//   showLoader();
 
-  getImagesByQuery(query)
-    .then(data => {
-      if (data.hits.length > 0) {
-        createGallery(data.hits);
-      } else {
-        iziToast.error({
-          title: 'Error!',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      }
-    })
-    .catch(() => {
-      iziToast.error({
-        title: 'Error!',
-        message: 'Failed to load images',
-      });
-    })
-    .finally(hideLoader);
-});
+//   getImagesByQuery(query)
+//     .then(data => {
+//       if (data.hits.length > 0) {
+//         createGallery(data.hits);
+//       } else {
+//         iziToast.error({
+//           title: 'Error!',
+//           message:
+//             'Sorry, there are no images matching your search query. Please try again!',
+//         });
+//       }
+//     })
+//     .catch(() => {
+//       iziToast.error({
+//         title: 'Error!',
+//         message: 'Failed to load images',
+//       });
+//     })
+//     .finally(hideLoader);
+// });
